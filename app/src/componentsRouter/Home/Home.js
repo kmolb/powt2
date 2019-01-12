7f5945cae0135d9f516fbf82236904568c058657
Kamil Olber <olb.kamil@gmail.com>
2019-01-12 10:01:51 +0100

    wyciagniecie emaila z api

--- app/src/componentsRouter/Home/Home.js
+++ app/src/componentsRouter/Home/Home.js
@@ -1,14 +1,38 @@
 import React, { Component } from 'react';
 
 class Home extends Component {  
+  constructor(props){
+    super(props);
+
+    this.state = {
+      isLoading: true,
+      data: null,
+    }
+  }
   componentDidMount(){
     fetch("https://randomuser.me/api/")
     .then((resp) => {
+      return resp.json();
+    })
+    .then((resp) => {
+      this.setState({
+        isLoading:false,
+        data: resp,
+      })
       console.log(resp);
     })
   } 
+
   render(){
-    return <h1>Home</h1>
+    const { isLoading, data } = this.state;
+    return <div><h1>Home</h1>
+    {isLoading && <div>Loading</div>}
+    {data && <div>
+      {data.results[0].email}
+    </div>}
+    </div>
+    
+   
   }
 }
 
